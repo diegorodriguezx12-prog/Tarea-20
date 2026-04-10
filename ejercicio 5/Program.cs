154e09e69a34ff2ff5ea3e2bd397decce9e7fe24
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:47:13 -0600

    ejercicio 5 terminado

--- ejercicio 5/Program.cs
+++ ejercicio 5/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de números (n): ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese valor para posición " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int mayor = numeros[0];
+
+for (int i = 1; i < numeros.Length; i++)
+{
+    if (numeros[i] > mayor)
+    {
+        mayor = numeros[i];
+    }
+}
+
+Console.WriteLine("Mayor: " + mayor);
