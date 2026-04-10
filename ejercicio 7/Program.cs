21dfb82e5b50fd78dbcb8ca0888630d2d54f27ae
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:02:17 -0600

    ejercicio 7 terminado

--- ejercicio 7/Program.cs
+++ ejercicio 7/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+int pares = 0, impares = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    if (numeros[i] % 2 == 0) { pares++; }
+    else { impares++; }
+}
+Console.WriteLine("Pares: " + pares);
+Console.WriteLine("Impares: " + impares);
