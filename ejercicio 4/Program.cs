4633b991b4110613a577fbf5f783a910b30f9f08
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:36:18 -0600

    ejercicio 4 terminado

--- ejercicio 4/Program.cs
+++ ejercicio 4/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de números (n): ");
+int n = int.Parse(Console.ReadLine());
+
+double[] numeros = new double[n];
+double sumaTotal = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese el valor para la posición " + i + ": ");
+    numeros[i] = double.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    sumaTotal += numeros[i];
+}
+
+double promedio = sumaTotal / n;
+
+Console.WriteLine("La suma total es: " + sumaTotal);
+Console.WriteLine("El promedio es: " + promedio);
