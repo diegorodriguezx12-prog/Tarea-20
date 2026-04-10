fd33134f74b3eaffa64191662f4776199e38f99a
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:28:07 -0600

    ejercicio 3 terminado

--- ejercicio 3/Program.cs
+++ ejercicio 3/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de números (n): ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+int suma = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese el valor para la posición " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    suma += numeros[i]; 
+}
+
+Console.WriteLine("\nLa suma total es: " + suma);
