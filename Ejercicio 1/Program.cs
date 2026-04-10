431c7e25dbed3e6ce4dc4eaac4c6de2a119ef8b0
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 21:58:03 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese la cantidad de números que desea ingresar");
+int cantidad = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese el valor para la posición " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
