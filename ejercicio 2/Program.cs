0c27c4a1290be039520470a598dfdd421aea1864
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:20:39 -0600

    ejercicio 2 terminado

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de números (n): ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese el valor para la posición " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Mostrando los datos en orden inverso:");
+
+for (int i = numeros.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine("Posición: " + i + "  Valor: " + numeros[i]);
+}
