4f8accd8e9aeda77d591bef4940f389cba451f53
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:23:38 -0600

    ejercicio 10 terminado

--- ejercicio 10/Program.cs
+++ ejercicio 10/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Positivos:");
+for (int i = 0; i < n; i++)
+{
+    if (numeros[i] > 0) { Console.WriteLine(numeros[i]); }
+}
