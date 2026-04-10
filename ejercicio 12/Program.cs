b4b62b46c38b5e38a06e07850936bfd662f7d46b
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:35:15 -0600

    ejercicio 12 terminado

--- ejercicio 12/Program.cs
+++ ejercicio 12/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < n - 1; i++)
+{
+    for (int j = i + 1; j < n; j++)
+    {
+        if (numeros[i] > numeros[j])
+        {
+            int temp = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = temp;
+        }
+    }
+}
