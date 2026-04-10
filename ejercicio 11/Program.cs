8f8a07fb63e3822a51ad0ba37801f3cf8b0b0ef2
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:29:23 -0600

    ejercicio 11 terminado

--- ejercicio 11/Program.cs
+++ ejercicio 11/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] original = new int[n];
+int[] copia = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    original[i] = int.Parse(Console.ReadLine());
+    copia[i] = original[i];
+}
