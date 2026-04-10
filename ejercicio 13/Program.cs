3b0c448b870e6afde7a22452c449c2c4fdfe920e
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:41:34 -0600

    ejercicio 13 terminado

--- ejercicio 13/Program.cs
+++ ejercicio 13/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Pares:");
+for (int i = 0; i < n; i++) { if (numeros[i] % 2 == 0) Console.Write(numeros[i] + " "); }
+
+Console.WriteLine("\nImpares:");
+for (int i = 0; i < n; i++) { if (numeros[i] % 2 != 0) Console.Write(numeros[i] + " "); }
