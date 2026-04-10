560cf3e348835433d51cfb971a552a227effb179
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:55:13 -0600

    ejercicio 6 terminado

--- ejercicio 6/Program.cs
+++ ejercicio 6/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int menor = numeros[0];
+for (int i = 1; i < numeros.Length; i++)
+{
+    if (numeros[i] < menor)
+    {
+        menor = numeros[i];
+    }
+}
+Console.WriteLine("Menor: " + menor);
