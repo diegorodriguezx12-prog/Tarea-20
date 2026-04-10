387916b6a37c1c35bf736c2d125b522f4ad77cdd
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:16:22 -0600

    ejercicio 9 terminado

--- ejercicio 9/Program.cs
+++ ejercicio 9/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Posición a modificar: ");
+int pos = int.Parse(Console.ReadLine());
+Console.Write("Nuevo valor: ");
+numeros[pos] = int.Parse(Console.ReadLine());
