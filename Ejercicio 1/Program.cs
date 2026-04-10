6f27b9d9d64f41db29f30b10d35f201498484873
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 22:06:11 -0600

    ejercicio 1 terminado

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -8,3 +8,8 @@ for (int i = 0; i < numeros.Length; i++)
     Console.Write("Ingrese el valor para la posición " + i + ": ");
     numeros[i] = int.Parse(Console.ReadLine());
 }
+Console.WriteLine("\nMostrando los datos guardados:");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Posición: " + i + "  Valor: " + numeros[i]);
+}
