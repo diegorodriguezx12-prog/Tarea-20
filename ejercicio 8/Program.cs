23c9026ffefbffc7786592e979b4aed31d64ec94
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-09 23:09:16 -0600

    ejercicio 8 terminado

--- ejercicio 8/Program.cs
+++ ejercicio 8/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + i + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Valor a buscar: ");
+int buscar = int.Parse(Console.ReadLine());
+int repeticiones = 0;
+bool encontrado = false;
+
+for (int i = 0; i < n; i++)
+{
+    if (numeros[i] == buscar)
+    {
+        encontrado = true;
+        repeticiones++;
+    }
+}
+Console.WriteLine("¿Encontrado?: " + encontrado);
+Console.WriteLine("Repeticiones: " + repeticiones);
